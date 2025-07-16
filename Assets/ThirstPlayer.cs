03f19a939eda43c3bddabb045b30a87cb6a65e7c
hahnspena102 <hahns.pena@gmail.com>
2025-07-15 20:28:56 -0700

    thirst base

---
+++ Assets/ThirstPlayer.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class ThirstPlayer : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float verticalInput;
+    private float speed = 8f;
+    private float jumpHeight = 10f;
+    private bool onGround = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxisRaw("Horizontal");
+
+        if (rb)
+        {
+            rb.linearVelocity = new Vector2(verticalInput * speed, rb.linearVelocity.y);
+        }
+
+        
+    }
+
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.Space) && onGround)
+        {
+            Jump();
+        } 
+    }
+
+    void Jump()
+    {
+        Debug.Log("jmp");
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+        onGround = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+}
